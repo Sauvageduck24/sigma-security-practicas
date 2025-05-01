f7e25b5cea863afc4b9ac0e45e13fe39ed27d77b
Sauvageduck24 <estebansanchezgamez@gmail.com>
2025-05-01 11:46:28 +0200

    p5

--- api/engine.py
+++ api/engine.py
@@ -2,8 +2,11 @@ from sqlalchemy import create_engine
 
 # Define the URI in a single location
 
-#uri = 'mysql+pymysql://root:1234@localhost/sigma_security'
-uri = 'mysql+pymysql://sauvageduck24:sigmasecurity@sauvageduck24.mysql.pythonanywhere-services.com/sauvageduck24$sigmasecurity'
+uri = 'mysql+pymysql://root:1234@localhost/sigma_security'
+#uri = 'mysql+pymysql://sauvageduck24:sigmasecurity@sauvageduck24.mysql.pythonanywhere-services.com/sauvageduck24$sigmasecurity'
 
 # Create the shared engine
-engine = create_engine(uri)
+engine = create_engine(uri,pool_size=10,
+    max_overflow=20,
+    pool_timeout=30,
+    pool_recycle=1800)
